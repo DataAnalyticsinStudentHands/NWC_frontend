b248bbd0c4c5aa93eed6c09458ba754d61dcbc4e
Peggy Lindner <webadmin@dash.cs.uh.edu>
2022-03-05 13:10:32 -0600

    Hotfix Updates for AboutProject page to use new Strapi content type structure

--- src/Pages/AboutProject/About.js
+++ src/Pages/AboutProject/About.js
@@ -7,170 +7,112 @@ import tl from './res/tl.png';
 import meet from './res/meatteem.png';
 import bottomchart from './res/bottomchart.png';
 
-import axios from '../../config/axios';
 import VARIABLES from "../../config/.env.js";
-import { ArrowCounterclockwise } from 'react-bootstrap-icons';
-
-const getWhere = (data, key, value) => {
-  return data.filter(e => e[key] === value);
-}
-
-const urlify = (str) => {
-  return [VARIABLES.fetchBaseUrl, str].join('/');//VARIABLES.axiosBaseURL.slice(0, VARIABLES.axiosBaseURL.length-1) + "" + str;
-}
 
 function About() {
-  const jack = "_";//"ALL WORK AND NO PLAY MAKES JACK A DULL BOY. ALL WORK AND NO PLAY MAKES JACK A DULL BOY. ALL WORK AND NO PLAY MAKES JACK A DULL BOY.";
-  const yt = "https://www.youtube.com";
+  const { fetchBaseUrl } = VARIABLES;
 
-  const [aboutBanner_card, setAboutBanner_card] = useState(jack);
-  const [aboutImgCredit, setAboutImgCredit] = useState("JANE DOE");
-  const [aboutImgCredit_more, setAboutImgCredit_more] = useState("")
+  // one state to hold the regular page content loaded from Strapi
+  const [state, setState] = useState({
+    aboutBanner_card: '',
+    bannerimage_credit: '',
+    bannerimagecredit_more: '',
+    aboutTimeline_1: '',
+    aboutTimeline_2: '',
+    aboutTimeline_3: '',
+    aboutTimeline_4: '',
+    aboutTimeline_5: '',
+    aboutDocuments_ddlink: '',
+    aboutDocuments_cblink: '',
+    aboutDocuments_frlink: ''
+  });
 
-  const [aboutTimeline_1, setAboutTimeline_1] = useState(jack);
-  const [aboutTimeline_2, setAboutTimeline_2] = useState(jack);
-  const [aboutTimeline_3, setAboutTimeline_3] = useState(jack);
-  const [aboutTimeline_4, setAboutTimeline_4] = useState(jack);
-  const [aboutTimeline_5, setAboutTimeline_5] = useState(jack);
-
-  const [aboutDocuments_ddlink, setAboutDocuments_ddlink] = useState(jack);
-  const [aboutDocuments_cblink, setAboutDocuments_cblink] = useState(jack);
-  const [aboutDocuments_aplink, setAboutDocuments_aplink] = useState(jack);
-  const [aboutDocuments_frlink, setAboutDocuments_frlink] = useState(jack);
-
+  // grab page data from strapi
   useEffect(() => {
-    fetch([VARIABLES.fetchBaseUrl, "content-abouts"].join('/'))
-    .then(res => res.json())
-    .then(data => {
-      const get = (section) => {
-        return getWhere(data, 'Section', section)[0]['Content'];
-      };
-
-      setAboutBanner_card (
-        get("aboutBanner_card")
-      );
-
-      setAboutImgCredit(
-        get("aboutImgCredit")
-      );
-
-      setAboutImgCredit_more(
-          get("aboutImgCredit_more")
-      )
-
-      setAboutTimeline_1 (
-        get("aboutTimeline_1")
-      );
-
-      setAboutTimeline_2 (
-        get("aboutTimeline_2")
-      );
-
-      setAboutTimeline_3 (
-        get("aboutTimeline_3")
-      );
-
-      setAboutTimeline_4 (
-        get("aboutTimeline_4")
-      );
-
-      setAboutTimeline_5(
-        get("aboutTimeline_5")
-      );
-    })
-  }, []);
-
-  useEffect(() => {
-    fetch([VARIABLES.fetchBaseUrl, "content-about-pdfs"].join('/'))
-    .then(res => res.json())
-    .then(data => {
-      const get = (section) => {
-        return urlify(getWhere(data, 'Section', section)[0]["pdf"][0]["url"]);
-      };
-
-      setAboutDocuments_ddlink(
-        get("aboutDocuments_ddlink")
-      );
-
-      setAboutDocuments_cblink(
-        get("aboutDocuments_cblink")
-      );
-
-      setAboutDocuments_frlink(
-        get("aboutDocuments_frlink")
-      );
-
-      setAboutDocuments_aplink(
-        get("aboutDocuments_aplink")
-      );
-    })
-    .catch(err => console.log(err));
-  }, []);
+    fetch(`${fetchBaseUrl}/content-about`)
+      .then(res => res.json())
+      .then(data => {
+        setState({
+          banner_text: data.Banner_text,
+          bannerimage_credit: data.BannerImage_Credit,
+          bannerimagecredit_more: data.BannerImageCredit_more,
+          aboutTimeline_1: data.aboutTimeline_1,
+          aboutTimeline_2: data.aboutTimeline_2,
+          aboutTimeline_3: data.aboutTimeline_3,
+          aboutTimeline_4: data.aboutTimeline_4,
+          aboutTimeline_5: data.aboutTimeline_5,
+          aboutDocuments_ddlink: data.aboutDocuments_ddlink.length !== 0 ? fetchBaseUrl + data.aboutDocuments_ddlink[0].url : undefined,
+          aboutDocuments_cblink: data.aboutDocuments_cblink.length !== 0 ? fetchBaseUrl + data.aboutDocuments_cblink[0].url : undefined,
+          aboutDocuments_frlink: data.aboutDocuments_frlink.length !== 0 ? fetchBaseUrl + data.aboutDocuments_frlink[0].url : undefined
+        });
+      })
+      .catch(err => console.log(err));
+  }, []); // eslint-disable-line
 
   return (
     <div className="about">
-      
+
       {/**BANNER */}
       <div className="aboutBanner">
-        <img src={button} className="aboutBanner_button"/>
+        <img src={button} className="aboutBanner_button" alt="_" />
         <div className="aboutBanner_card">
           <p>
-            {aboutBanner_card}
+            {state.banner_text}
           </p>
         </div>
-        <div className="aboutBanner_credit" title={aboutImgCredit_more}><p>PHOTO BY {aboutImgCredit}</p></div>
-        <img src={chick} className="aboutBanner_chick"/>
+        <div className="aboutBanner_credit" title={state.bannerimagecredit_more}><p>PHOTO BY {state.bannerimage_credit}</p></div>
+        <img src={chick} className="aboutBanner_chick" alt="_" />
       </div>
-    
+
       {/**TIMELINE */}
       <div className="aboutTimeline">
         <div className="aboutTimeline_header"><p>PROJECT TIMELINE</p></div>
         <div className="aboutTimeline_annot aboutTimeline_annot1">
           <div className="aboutTimeline_annotNo"><p>1</p></div>
-          <div className="aboutTimeline_annotText">{aboutTimeline_1}</div>
+          <div className="aboutTimeline_annotText">{state.aboutTimeline_1}</div>
         </div>
         <div className="aboutTimeline_annot aboutTimeline_annot2">
           <div className="aboutTimeline_annotNo"><p>2</p></div>
-          <div className="aboutTimeline_annotText">{aboutTimeline_2}</div>
+          <div className="aboutTimeline_annotText">{state.aboutTimeline_2}</div>
         </div>
         <div className="aboutTimeline_annot aboutTimeline_annot3">
           <div className="aboutTimeline_annotNo"><p>3</p></div>
-          <div className="aboutTimeline_annotText">{aboutTimeline_3}</div>
+          <div className="aboutTimeline_annotText">{state.aboutTimeline_3}</div>
         </div>
         <div className="aboutTimeline_annot aboutTimeline_annot4">
           <div className="aboutTimeline_annotNo"><p>4</p></div>
-          <div className="aboutTimeline_annotText">{aboutTimeline_4}</div>
+          <div className="aboutTimeline_annotText">{state.aboutTimeline_4}</div>
         </div>
         <div className="aboutTimeline_annot aboutTimeline_annot5">
           <div className="aboutTimeline_annotNo"><p>5</p></div>
-          <div className="aboutTimeline_annotText">{aboutTimeline_5}</div>
+          <div className="aboutTimeline_annotText">{state.aboutTimeline_5}</div>
         </div>
-        <img src={tl} className="aboutTimeline_tl"/>
+        <img src={tl} className="aboutTimeline_tl" alt="_" />
       </div>
 
       {/**DOCUMENTS */}
       <div className="aboutDocuments">
-        <a href={aboutDocuments_ddlink} target="_blank">
-        <div className="aboutDocuments_item aboutDocuments_design">
-          DESIGN DOCUMENTS
-        </div>
-        </a>
-        <a href={aboutDocuments_cblink} target="_blank">
-        <div className="aboutDocuments_item aboutDocuments_code">
-          CODEBOOK
-        </div>
-        </a>
-        {/*<a href={aboutDocuments_aplink} target="_blank">
-        <div className="aboutDocuments_item aboutDocuments_approach">
-          APPROACH TO DIGITAL HUMANITIES
-        </div>
-        </a>*/}
-        <a href={aboutDocuments_frlink} target="_blank">
-        <div className="aboutDocuments_item aboutDocuments_further">
-          FURTHER READING
-        </div>
-        </a>
-
+        {state.aboutDocuments_ddlink &&
+          <a href={state.aboutDocuments_ddlink} target="_blank" rel="noreferrer" >
+            <div>
+              DESIGN DOCUMENTS
+            </div>
+          </a>
+        }
+        {state.aboutDocuments_cblink &&
+          <a href={state.aboutDocuments_cblink} target="_blank" rel="noreferrer" >
+            <div>
+              CODEBOOK
+            </div>
+          </a>
+        }
+        {state.aboutDocuments_frlink &&
+          <a href={state.aboutDocuments_frlink} target="_blank" rel="noreferrer" >
+            <div>
+              FURTHER READING
+            </div>
+          </a>
+        }
       </div>
 
       {/**MEET */}
@@ -182,7 +124,7 @@ function About() {
       <div className="aboutChart">
         <img src={bottomchart}/>
       </div>
-    
+
     </div>
   )
 }
