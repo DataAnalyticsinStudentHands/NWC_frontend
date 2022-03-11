4ad0b201d3dafe0615d8764d7f18f3df394cddc8
Patrick Kurth <patrickwkurth@gmail.com>
2022-03-11 09:16:32 -0600

    Added pagination component

--- src/Pages/Discover/Discover.js
+++ src/Pages/Discover/Discover.js
@@ -3,18 +3,23 @@ import "./Discover.css";
 import discoverButton from "../../res/imgs/buttondiscover.png";
 import LCard from '../../Components/LCard/LCard';
 import CaptionedImg from '../../Components/CaptionedImg/CaptionedImg';
-import FeaturedCard from "../../Components/FeaturedCard/FeaturedCard";
 import discoverbannerperson from "../../res/imgs/discoverbannerperson.png";
 import VARIABLES from "../../config/.env.js";
 import { loadcards } from './cardloader';
 import DiscoverCard from '../../Components/DiscoverCard/DiscoverCard';
 import { getSafe, processPageOld } from "../../Components/util/util";
 import { Link } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
 
 function Discover() {
+  const [dataLength, setDataLength] = useState()
   const [cards, setCards] = useState([]);
+  const [featuredCards, setFeatured] = useState([])
+  const [currentPage, setCurrentPage] = useState(0);
+  const [postsPerPage, setPostsPerPage] = useState(12);
   const [input, setInput] = useState("");
-
+  let totalPages = (Math.ceil(dataLength / postsPerPage))
+  
   const [stateOld, setStateOld] = useState({
     bannerText: "abcdefg",
   }); // Handles the text throughout page.
@@ -22,12 +27,29 @@ function Discover() {
   const { fetchBaseUrl } = VARIABLES;
 
   useEffect(() => {
-    fetch([fetchBaseUrl, `content-discover-stories/?_limit=12`/* + `?_start=${page}&_limit=2`*/].join('/'))
+    fetch([fetchBaseUrl, `content-discover-stories?_limit=-1`/* + `?_start=${page}&_limit=2`*/].join('/'))
+      .then(response => response.json())
+      .then(data => {
+        loadcards(data, setFeatured);
+      })
+      .catch(err => console.log(err));
+  }, []); // eslint-disable-line
+
+  useEffect(() => {
+    fetch([fetchBaseUrl, `content-discover-stories?_start=${currentPage}&_limit=${postsPerPage}`/* + `?_start=${page}&_limit=2`*/].join('/'))
       .then(response => response.json())
       .then(data => {
         loadcards(data, setCards);
       })
-      .then(e => console.log(cards))
+      .catch(err => console.log(err));
+  }, [currentPage, postsPerPage]); // eslint-disable-line
+
+  useEffect(() => {
+    fetch([fetchBaseUrl, `content-discover-stories?_limit=-1`/* + `?_start=${page}&_limit=2`*/].join('/'))
+      .then(response => response.json())
+      .then(dataLength => {
+        setDataLength(dataLength.length)
+      })
       .catch(err => console.log(err));
   }, []); // eslint-disable-line
 
@@ -65,6 +87,17 @@ function Discover() {
       .then(data => loadcards(data, setCards))
       .catch(err => console.log(err));
   }
+  
+
+  //Cards shown amount
+  function handleSelectChange(e) {
+    setPostsPerPage(e.target.value);
+  }
+
+  //Pagination handleClick
+  function handlePageClick(e) {
+    setCurrentPage(e.selected)
+  }
 
   return (
     <div className="discover">
@@ -83,7 +116,7 @@ function Discover() {
       <div className="discoverFeatured">
         <h2>FEATURED STORIES</h2>
         <div className="discoverFeatured_cards">
-          {cards
+          {featuredCards
             .filter(value => value.featured === 'true')
             .map((value, index) => <a href={`/discover/${value.id}`}><DiscoverCard
               key={Math.random()}
@@ -119,15 +152,35 @@ function Discover() {
           <div className="discoverButtons_participants">VIEW PARTICIPANTS</div>
         </Link>
       </div>
+      <div className="cardsPerPage">
+          <div className="cardsPerPageHeader">
+          <h3>Cards per page</h3>
+            </div>
+      <ul className="cardsListPerPage">
+            <button onClick={handleSelectChange} value={12}>12</button>
+            <button onClick={handleSelectChange} value={24}>24</button>
+            <button onClick={handleSelectChange} value={48}>48</button>
+            <button onClick={handleSelectChange} value={96}>96</button>
+        </ul>
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel="next >"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={2}
+            marginPagesDisplayed={3}
+            pageCount={totalPages}
+            previousLabel="< previous"
+            containerClassName={"pagination"}
+            previousLinkClassName={"pagination__link"}
+            nextLinkClassName={"pagination__link"}
+            disabledClassName={"pagination__link--disabled"}
+            activeClassName={"pagination__link--active"}
+            />
+      </div>
 
       {/**CARDS */}
-      <div className="discoverCards_border"></div>
-      {/*<button type="button" onClick={() => page == 2 ? setPage(0) : setPage(2)}>
-                Change the Page
-            </button>*/}
-
       <div className="discoverCards">
-        {cards.map((value, index) => <DiscoverCard
+        {cards.map((value) => <DiscoverCard
           key={Math.random()}
           color={["yellow", "blue", "red", "teal"][value.name.charCodeAt(0) % 4]}
           href={`/discover/${value.id}`}
@@ -138,7 +191,6 @@ function Discover() {
         />)
         }
       </div>
-
     </div>
   )
 }
