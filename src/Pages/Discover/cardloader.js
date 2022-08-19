3a85ad6b8e4f5fd114ee8948cbc5435baf5093cc
Gabino Aguirre <gaguirr5@cougarnet.uh.edu>
2022-08-19 18:59:20 -0500

    change hardcode api fetch to fetchBaseUrl

--- src/Pages/Discover/cardloader.js
+++ src/Pages/Discover/cardloader.js
@@ -1,12 +1,15 @@
+import VARIABLES from "../../config/.env.js";
+const { fetchBaseUrl } = VARIABLES;
+
 function d2card(datum) {
     let profilepic = ""
     // let profilepic2 = ''
     if(datum.attributes.profilepic.data === null){
         profilepic = null
     }else{
-        profilepic = `https://dash.cs.uh.edu${datum.attributes.profilepic.data.attributes.formats.thumbnail.url}`
+        // profilepic = `https://dash.cs.uh.edu${datum.attributes.profilepic.data.attributes.formats.thumbnail.url}
+        profilepic = `${fetchBaseUrl}${datum.attributes.profilepic.data.attributes.formats.thumbnail.url}`
 
-        //  profilepic2 = `http://localhost:1337${datum.attributes.profilepic.data.attributes.formats.thumbnail.url}`
     }
     // datum.profilepic.length > 0
     // if(check === false){
