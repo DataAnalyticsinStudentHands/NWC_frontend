f6675f40b3bbe9cb8df00b5a6923ac4133d33cec
peggylind <peggylind@gmail.com>
2023-05-18 13:54:26 -0500

    prep for production, added _production files to be able to use main/develop without changes

---
+++ src/App_production.js
@@ -0,0 +1,106 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import './Custom.css';
+import Footer from "./Components/Footer/Footer";
+import Navigation from './Components/Navigation/Navigation';
+import Home from './Pages/Home/Home';
+import About from "./Pages/AboutProject/About";
+import Why from "./Pages/Why/Why";
+import Essay from "./Pages/Essay/Essay";
+import Participants from "./Pages/Participants/Participants";
+import Discover from "./Pages/Discover/Discover";
+import DiscoverInfo from "./Pages/Discover/DiscoverInfo";
+import ResearchingNWC from "./Pages/ResearchingNWC/ResearchingNWC";
+import MeetTheTeam from "./Pages/MeetTheTeam/MeetTheTeam";
+import HowToContribute from "./Pages/HowToContribute/HowToContribute";
+import HowToDonatePapersForm from './Pages/Forms/HowToDonatePapers';
+import CorrectionsForm from './Pages/Forms/CorrectionsForm';
+import ContactUsForm from "./Pages/Forms/ContactUsForm";
+import MoreIdeasForm from './Pages/Forms/MoreIdeasForm';
+import PDFViewer from './Pages/PDFViewer/PDFViewer';
+import ScrollToTop from "./Components/util/ScrollToTop";
+import ResourceResearchers from "./Pages/HowToContribute/ResourceResearchers/ResourceResearchers"
+import ResourceArchivists from "./Pages/HowToContribute/ResourceArchivists/ResourceArchivists"
+import ResourceStudents from "./Pages/HowToContribute/ResourceStudents/ResourceStudents"
+import ResourceNWC from "./Pages/HowToContribute/ResourceNWC/ResourceNWC"
+import ResourceEducators from "./Pages/HowToContribute/ResourceEducators/ResourceEducators"
+
+function App() {
+  return (
+    <Router className="App">
+      <Navigation />
+      <ScrollToTop>
+      <Switch>
+        <Route path="/MeetTheTeam">
+          <MeetTheTeam />
+        </Route>
+        <Route path="/DiscoverNWCStories">
+          <Discover />
+        </Route>
+        <Route path="/discover/:storyId">
+          <DiscoverInfo />
+        </Route>
+        <Route path="/Discover">
+          <Discover />
+        </Route>
+        <Route path="/Why">
+          <Why />
+        </Route>
+        <Route path="/Essay">
+          <Essay />
+        </Route>
+        <Route path="/HowToContribute">
+          <HowToContribute />
+        </Route>
+        <Route path="/About">
+          <About />
+        </Route>
+        <Route path="/Participants">
+          <Participants />
+        </Route>
+        <Route path="/PDFViewer/:pdffile">
+          <PDFViewer />
+        </Route>
+        <Route path="/Forms/CorrectionsForm">
+          <CorrectionsForm />
+        </Route>
+        <Route path="/Forms/ContactUsForm">
+          <ContactUsForm />
+        </Route>
+        <Route path="/Forms/HowToDonatePapersForm">
+          <HowToDonatePapersForm />
+        </Route>
+        <Route path="/Forms/MoreIdeasForm">
+          <MoreIdeasForm />
+        </Route>
+        <Route path="/ResourceResearchers">
+          <ResourceResearchers />
+        </Route>
+        <Route path="/ResourceArchivists">
+          <ResourceArchivists />
+        </Route>
+        <Route path="/ResourceStudents">
+          <ResourceStudents />
+        </Route>
+        <Route path="/ResourceNWC">
+          <ResourceNWC />
+        </Route>
+        <Route path="/ResourceEducators">
+          <ResourceEducators />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+      </ScrollToTop>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Components/Navigation/Navigation_production.js
@@ -0,0 +1,38 @@
+import './Navigation.css';
+import { Link, useLocation } from 'react-router-dom';
+
+function Navigation() {
+  const location = useLocation();
+
+  function JuicyLink({ navname, pathname }) {
+    return (
+      <Link
+        className={`navigation_link ${
+          location.pathname === pathname ? 'navigation_link--selected' : ''
+        }`}
+        to={pathname}
+      >
+        {navname}
+      </Link>
+    );
+  }
+
+  return (
+    <div className="navigation_total">
+      <nav className="navigation">
+        <div></div>
+        <JuicyLink navname="Home" pathname="/" />
+        <JuicyLink navname="About Project" pathname="/About" />
+        <JuicyLink navname="Why The NWC Matters" pathname="/Why" />
+        <JuicyLink navname="Discover NWC Stories" pathname="/DiscoverNWCStories"/>
+        <JuicyLink navname="How to Contribute" pathname="/HowToContribute" />
+        <div></div>
+        {/*
+        <img className="navigation_search" src={icon_search}/>*/}
+      </nav>
+      <div className="navigation_border"></div>
+    </div>
+  );
+}
+
+export default Navigation;
