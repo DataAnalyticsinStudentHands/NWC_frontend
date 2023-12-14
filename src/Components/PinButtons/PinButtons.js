8bf4305037319fd1aef656133f500c2bb8ea5289
Thang Nguyen <nguyenn.thang@yahoo.com>
2023-12-14 07:54:28 -0600

    PinButtons new temp design

--- src/Components/PinButtons/PinButtons.js
+++ src/Components/PinButtons/PinButtons.js
@@ -6,15 +6,36 @@ export const PinButtons = (props) => {
   const { data, buttons } = props;
 
   return (
-    <Stack direction="row" className="homeButtons" margin="6% 6%" spacing={20} wrap>
-      {buttons.map((button, index) => ( //for each button
-        <Link key={index} to={data[`homeButton${index + 1}_link`]}> {/* Link each img and text to the button */}
-          <Stack direction="row" className="homeButtons_button">
-            <img src={button} alt={`button_${index + 1}`} />
-            <p>{data[`homeButton${index + 1}_text`]}</p>
-          </Stack>
-        </Link>
-      ))}
-    </Stack>
+
+    <Stack direction='column' gap={4} margin={'8% 5%'} className="pinButtons">
+    {Object.keys(buttons).map((key, index) => {
+        return (
+          <Link key={index} to={data[`homeButton${index + 1}_link`]}> {/* Link each img and text to the button */}
+            <Stack direction='row' className='item' key={index} margin={'0% 5%'}>
+                <div className="item-left">
+                  <img src={buttons[key]} alt={`button_${index + 1}`} />
+                </div>
+                <div className="item-right">
+                  <p>{data[`homeButton${index + 1}_text`]}</p>
+                </div>
+            </Stack>
+            </Link>
+        );
+    })}
+</Stack>
+    // <Stack direction="row" className="pinButtons" >
+    //   {buttons.map((button, index) => ( //for each button
+    //     <Link key={index} to={data[`homeButton${index + 1}_link`]}> {/* Link each img and text to the button */}
+    //       <Stack direction="row" className="item" margin="16% 10%" >
+    //         <div className="item-left">
+    //         <img src={button} alt={`button_${index + 1}`} />
+    //         </div>
+    //         <div className="itemr-ight">
+    //         <p>{data[`homeButton${index + 1}_text`]}</p>
+    //           </div>
+    //       </Stack>
+    //     </Link>
+    //   ))}
+    // </Stack>
   );
 };
