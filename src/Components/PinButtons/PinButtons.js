1611b0b04512ef9e80d641159b781fff72f03c36
Thang Nguyen <nguyenn.thang@yahoo.com>
2024-02-29 07:31:11 -0600

    Mobile layout changes

--- src/Components/PinButtons/PinButtons.js
+++ src/Components/PinButtons/PinButtons.js
@@ -1,14 +1,14 @@
-import './PinButtons.scss';
+import './PinButtons.css';
 import { Link } from 'react-router-dom';
 import { Stack } from '../../Components/Stack';
 
 export const PinButtons = ( { button, link, text}) => {
   return (
     <Link to={link}>
-      <Stack className="item_pin">
+      <div className="item_pin">
         <img src={button} alt="button" className="item-left_pin"/>
         <p className="item-right_pin"> {text} </p>
-      </Stack>
+      </div>
     </Link>
   );
 };
