d81e8d325ba66ba4d3edeada40085ce6094fb5f1
Thang Nguyen <nguyenn.thang@yahoo.com>
2025-06-26 09:03:02 -0500

    - Oral Histories 3.0 WIP

--- src/Pages/OralHistories/OralHistories.js
+++ src/Pages/OralHistories/OralHistories.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import video_placeholder from "./res/video_placeholder.png"
 import oral_histories_bannerpic from './res/oral_histories_bannerpic.png';
 import oral_histories_button from "../../assets/res/button-oral-histories.png";
@@ -6,15 +6,12 @@ import bible_women from './res/bible_women.png'
 import pro_plan_progress from './res/pro_plan_progress.png'
 // import placeholder from './res/placeholder.png'
 // import Carousel from './components/Carousel';
-import { ParticipantsTable } from './components/ParticipantsTable';
+// import { ParticipantsTable } from './components/ParticipantsTable';
 import { Banner } from "../../Components/Banner";
 import { Stack } from "../../Components/Stack";
 import { Typography } from "../../Components/Typography"
 import InfoVideo from "../../Components/Avalon/InfoVideo"
-import { loadcards } from '../Discover/cardloader';
-import DiscoverCard from '../Discover/Components/DiscoverCard';
-
-
+// import { loadcards } from '../Discover/cardloader';
 
 function OralHistories() {
 
@@ -27,12 +24,28 @@ function OralHistories() {
 
     });
     
-    // const [participants, setParticipants] = useState([])
-    const [roles, setRoles] = useState([]);
-    // states from Discover component
-    const [cards, setCards] = useState([])
+    // // const [participants, setParticipants] = useState([])
+    // const [roles, setRoles] = useState([]);
+    // // states from Discover component
+    // const [cards, setCards] = useState([])
     // const [dataLength, setDataLength] = useState()
-    const listOfCards = useRef([]);
+    // const listOfCards = useRef([]);
+
+    const tabs = [
+    "DELEGATES & ALTERNATES",
+    "DELEGATES AT LARGE",
+    "EXHIBITORS",
+    "INTERNATIONAL DIGNITARIES",
+    "JOURNALISTS",
+    "NATIONAL COMMISSIONERS",
+    "NOTABLE SPEAKERS",
+    "OBSERVERS",
+    "PAID STAFF MEMBERS",
+    "TORCH RELAY RUNNERS",
+    "VOLUNTEERS",
+    ];
+
+    const [activeTab, setActiveTab] = useState('All');
 
 
 
@@ -85,33 +98,33 @@ function OralHistories() {
     //         .catch((err) => console.log(err));
     //     }, []); // eslint-disable-line
 
-        useEffect(() => {
-        fetch([process.env.REACT_APP_API_URL, "api/nwc-roles?sort=role&populate=*"].join('/'))
-            .then((res) => res.json())
-            .then((data) => {
-            const filteredRoles = data.data
-            .filter((d) => d.attributes.OralHistory_Role_Toggle === true && !d.attributes.role.includes('Other Role')) //filter for toggle and 'Other Role'
-            .map((d) => d.attributes.role);
+        // useEffect(() => {
+        // fetch([process.env.REACT_APP_API_URL, "api/nwc-roles?sort=role&populate=*"].join('/'))
+        //     .then((res) => res.json())
+        //     .then((data) => {
+        //     const filteredRoles = data.data
+        //     .filter((d) => d.attributes.OralHistory_Role_Toggle === true && !d.attributes.role.includes('Other Role')) //filter for toggle and 'Other Role'
+        //     .map((d) => d.attributes.role);
     
-            // Check if "Other Role" is not already in the array and add it
-                if (!filteredRoles.includes("Other Role")) {
-                    filteredRoles.push("Other Role");
-                }
+        //     // Check if "Other Role" is not already in the array and add it
+        //         if (!filteredRoles.includes("Other Role")) {
+        //             filteredRoles.push("Other Role");
+        //         }
             
-                setRoles(filteredRoles);
-                })
-            .catch((err) => console.log(err));
-        }, []); // eslint-disable-line
+        //         setRoles(filteredRoles);
+        //         })
+        //     .catch((err) => console.log(err));
+        // }, []); // eslint-disable-line
 
-        useEffect(() => {
-            fetch(`${process.env.REACT_APP_API_URL}/api/content-discover-stories?_limit=-1&populate=*`)
-              .then(response => response.json())
-              .then(data => {
-                loadcards(data.data, setCards);
-                // setDataLength(data.data.length)
-                listOfCards.current = data.data
-              }).catch(err => console.log(err));
-          }, []); // eslint-disable-line
+        // useEffect(() => {
+        //     fetch(`${process.env.REACT_APP_API_URL}/api/content-discover-stories?_limit=-1&populate=*`)
+        //       .then(response => response.json())
+        //       .then(data => {
+        //         loadcards(data.data, setCards);
+        //         // setDataLength(data.data.length)
+        //         listOfCards.current = data.data
+        //       }).catch(err => console.log(err));
+        //   }, []); // eslint-disable-line
 
     return (
         <Stack direction='column' spacing={10}>
@@ -122,29 +135,73 @@ function OralHistories() {
                 imgRight={oral_histories_bannerpic}
                 imgCredit={state.imgCredit}
             />
-
-            {/* FEATURED */}
-            <Stack direction='column' className="OralHistories_Featured_container">
-                <div className="header-container">
-                    <Typography type="heading-1" paddingLR="0" paddingTB="0"> Featured Oral Histories </Typography>
-                    
-                </div>
-                <div className="discoverFeatured_cards">
-                    {cards
-                        .filter(value => value.featured === 'true')
-                        .map((value) => <DiscoverCard
-                        key={Math.random()}
-                        color={"teal"}
-                        href={`/Discover/${value.id}`}
-                        firstname={value.firstname}
-                        lastname={value.lastname}
-                        role={value.role}
-                        state={value.state}
-                        profilepic={value.profilepic}
-                        />)
-                }
-             </div>               
+            {/* EXPLORE ORAL HISTORIES */}
+            <Stack direction='column' gap={4} margin={'5% 5% 5% 5%'} className="OralHistories_NWC_container">
+                <Stack direction='row' className='item'>
+                    <div className="item-right">
+                        <Typography type="heading-2" paddingLR="0" paddingTB="0" fontSize="64"> Explore Oral Histories </Typography>
+                        <Typography type="paragraph-2" paddingLR="0" fontSize="18"> {state.exploreText} </Typography>
+                    </div>
+                </Stack>
             </Stack>
+
+            
+                <div className="tab-container">
+                    {/* Top-Left "All" Button */}
+                        <button
+                            className={`tab-button all-tab ${activeTab === 'All' ? 'active' : ''}`}
+                            onClick={() => setActiveTab('All')}
+                        >
+                            All
+                        </button>
+                    {/* Left Tabs */}
+                    <div className="tab-column left">
+                        {tabs.slice(0, 6).map((tab) => (
+                        <button
+                            key={tab}
+                            className={`tab-button left-tab ${activeTab === tab ? 'active' : ''}`}
+                            onClick={() => setActiveTab(tab)}
+                        >
+                            {tab}
+                        </button>
+                        ))}
+                    </div>
+
+                    {/* Right Tabs */}
+                    <div className="tab-column right">
+                        {tabs.slice(6).map((tab) => (
+                        <button
+                            key={tab}
+                            className={`tab-button right-tab ${activeTab === tab ? 'active' : ''}`}
+                            onClick={() => setActiveTab(tab)}
+                        >
+                            {tab}
+                        </button>
+                        ))}
+                    </div>
+                    </div>
+
+                          <div className="AdvancedSearch_border">  </div>
+           { /*
+           EXPLORE ORAL HISTORIES 
+           <Stack direction='column' gap={4} margin={'5% 5% 5% 5%'} className="OralHistories_NWC_container">
+                    <ParticipantsTable cards={cards} roles={roles} />
+            </Stack>
+            */}
+
+                        {/* WHAT THE NWC MEANS */}
+            <Stack direction='column' gap={4} margin={'5% 5% 5% 5%'} className="OralHistories_NWC_container">
+                <div className="reflection">
+                    <Typography type="heading-2" paddingLR="0" paddingTB="0"> Reflections </Typography>
+                    <Typography type="paragraph-2" paddingLR="10" paddingTB="1"> {state.meanText} </Typography>
+                </div>
+
+                <Stack direction='row' className='item'>
+                        <InfoVideo src={state.NWC_Means_VideoURL}/>
+                        <InfoVideo src={state.NWC_Means_VideoURL}/>
+                </Stack>
+            </Stack>
+
             {/* BANNER 2 */}
             <Stack direction='column' margin={'5% 0 0 0'} className="OralHistories_Voice_container">
                 <div className="centered-text">
@@ -159,30 +216,6 @@ function OralHistories() {
                             <p>Photos by {state.imgCredit}</p>
                         </div>
                     </Stack>
-            </Stack>
-
-            {/* EXPLORE ORAL HISTORIES */}
-            <Stack direction='column' gap={4} margin={'5% 5% 5% 5%'} className="OralHistories_NWC_container">
-                <Stack direction='row' className='item'>
-                    <div className="item-right">
-                        <Typography type="heading-2" paddingLR="0" paddingTB="0" fontSize="64"> Explore Oral Histories </Typography>
-                        <Typography type="paragraph-2" paddingLR="0" fontSize="18"> {state.exploreText} </Typography>
-                    </div>
-                </Stack>
-                    <ParticipantsTable cards={cards} roles={roles} />
-            </Stack>
-
-                        {/* WHAT THE NWC MEANS */}
-            <Stack direction='column' gap={4} margin={'5% 5% 5% 5%'} className="OralHistories_NWC_container">
-                <Stack direction='row' className='item'>
-                    <div className="item-left">
-                        <InfoVideo src={state.NWC_Means_VideoURL}/>
-                    </div>
-                    <div className="item-right">
-                        <Typography type="heading-2" paddingLR="0" paddingTB="0"> WHAT THE NWC MEANS TO ME </Typography>
-                        <Typography type="paragraph-2" paddingLR="0"> {state.meanText} </Typography>
-                    </div>
-                </Stack>
             </Stack>
         </Stack>
     );
