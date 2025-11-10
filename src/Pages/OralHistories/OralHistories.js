1e813574856de6b44edb91e20ee5b11fad538e5f
Thang Nguyen <nguyenn.thang@yahoo.com>
2025-11-10 09:55:01 -0600

    - Added tab icons and blobs

--- src/Pages/OralHistories/OralHistories.js
+++ src/Pages/OralHistories/OralHistories.js
@@ -4,16 +4,38 @@ import oral_histories_bannerpic from './res/oral_histories_bannerpic.png';
 import oral_histories_button from "../../assets/res/button-oral-histories.png";
 import bible_women from './res/bible_women.png'
 import pro_plan_progress from './res/pro_plan_progress.png'
-// import placeholder from './res/placeholder.png'
-// import Carousel from './components/Carousel';
-// import { ParticipantsTable } from './components/ParticipantsTable';
+
 import { Banner } from "../../Components/Banner";
 import { Stack } from "../../Components/Stack";
 import { Typography } from "../../Components/Typography"
 import InfoVideo from "../../Components/Avalon/InfoVideo"
 import Card from './components/Card'
 import { Pagination } from '../ResearchingNWC/Components/Pagination';
-// import { loadcards } from '../Discover/cardloader';
+
+//import explore oral history resources
+import da_icon from './res/explore/Delegates & Alternates - Nametag.png'
+import da_blob from './res/explore/Delegates & Alternates Tab Blob.svg'
+import dal_icon from './res/explore/Delegates at Large - US Map.png'
+import dal_blob from './res/explore/Delegates at Large Tab Blob.svg'
+import exhibitor_icon from './res/explore/Exhibitors - Booth.png'
+import exhibitor_blob from './res/explore/Exhibitors Tab Blob.svg'
+import dignitaries_icon from './res/explore/International Dignitaries - Globe.png' 
+import dignitaries_blob from './res/explore/International Dignitaries Tab blob.svg'
+import journalist_icon from './res/explore/Journalists - Newspaper.png'
+import journalist_blob from './res/explore/Journalists Tab Blob.svg'
+import commissioners_icon from './res/explore/National Commissioners - Gavel.png'
+import commissioners_blob from './res/explore/National Commissioners Tab Blob.svg'
+import speakers_icon from './res/explore/Notable Speakers - Microphone.png'
+import speakers_blob from './res/explore/Notable Speakers Tab Blob.svg'
+import observers_icon from './res/explore/Observers - Binoculars.png'
+import observers_blob from './res/explore/Observers Tab Blob.svg'
+import staff_icon from './res/explore/Paid Staff Members - Clipboard.png'
+import staff_blob from './res/explore/Paid Staff Members Tab Blob.svg'
+import torch_icon from './res/explore/Torch Relay Runners - Shoes.png'
+import torch_blob from './res/explore/Torch Relay Runners Tab Blob.svg'
+import volunteers_icon from './res/explore/Volunteers - Helping Hands.png'
+import volunteers_blob from './res/explore/Volunteers Tab Blob.svg'
+
 
 function OralHistories() {
 
@@ -26,26 +48,21 @@ function OralHistories() {
         Reflections_VideoURL2: '',
     });
     
-    // // const [participants, setParticipants] = useState([])
-    // const [roles, setRoles] = useState([]);
-    // // states from Discover component
-    // const [cards, setCards] = useState([])
-    // const [dataLength, setDataLength] = useState()
-    // const listOfCards = useRef([]);
 
     const tabs = [
-    "DELEGATES & ALTERNATES",
-    "DELEGATES AT LARGE",
-    "EXHIBITORS",
-    "INTERNATIONAL DIGNITARIES",
-    "JOURNALISTS",
-    "NATIONAL COMMISSIONERS",
-    "NOTABLE SPEAKERS",
-    "OBSERVERS",
-    "PAID STAFF MEMBERS",
-    "TORCH RELAY RUNNERS",
-    "VOLUNTEERS",
+        { name: "DELEGATES & ALTERNATES", icon: da_icon, blob: da_blob },
+        { name: "DELEGATES AT LARGE", icon: dal_icon, blob: dal_blob },
+        { name: "EXHIBITORS", icon: exhibitor_icon, blob: exhibitor_blob },
+        { name: "INTERNATIONAL DIGNITARIES", icon: dignitaries_icon, blob: dignitaries_blob },
+        { name: "JOURNALISTS", icon: journalist_icon, blob: journalist_blob },
+        { name: "NATIONAL COMMISSIONERS", icon: commissioners_icon, blob: commissioners_blob },
+        { name: "NOTABLE SPEAKERS", icon: speakers_icon, blob: speakers_blob },
+        { name: "OBSERVERS", icon: observers_icon, blob: observers_blob },
+        { name: "PAID STAFF MEMBERS", icon: staff_icon, blob: staff_blob },
+        { name: "TORCH RELAY RUNNERS", icon: torch_icon, blob: torch_blob },
+        { name: "VOLUNTEERS", icon: volunteers_icon, blob: volunteers_blob },
     ];
+
 
     const [activeTab, setActiveTab] = useState('All');
     
@@ -124,60 +141,67 @@ function OralHistories() {
                     </div>
                 </Stack>
             </Stack>
+            <div className="tab-container">
+                {/* Top-Left "All" Button */}
+                    <button
+                        className={`tab-button all-tab ${activeTab === 'All' ? 'active' : ''}`}
+                        onClick={() => setActiveTab('All')}
+                    >
+                        All
+                    </button>
+            {/* Left Tabs */}
+            <div className="tab-column left">
+            {tabs.slice(0, 6).map((tab) => (
+                <button
+                key={tab.name}
+                className={`tab-button left-tab ${activeTab === tab.name ? 'active' : ''}`}
+                onClick={() => setActiveTab(tab.name)}
+                >
+                <div className="tab-icon-wrapper">
+                    <img src={tab.blob} alt={`${tab.name} blob`} className="tab-blob" />
+                    <img src={tab.icon} alt={`${tab.name} icon`} className="tab-icon" />
+                </div>
+                <span className="tab-text">{tab.name}</span>
+                </button>
+            ))}
+            </div>
 
-            
-                <div className="tab-container">
-                    {/* Top-Left "All" Button */}
-                        <button
-                            className={`tab-button all-tab ${activeTab === 'All' ? 'active' : ''}`}
-                            onClick={() => setActiveTab('All')}
-                        >
-                            All
-                        </button>
-                    {/* Left Tabs */}
-                    <div className="tab-column left">
-                        {tabs.slice(0, 6).map((tab) => (
-                        <button
-                            key={tab}
-                            className={`tab-button left-tab ${activeTab === tab ? 'active' : ''}`}
-                            onClick={() => setActiveTab(tab)}
-                        >
-                            {tab}
-                        </button>
-                        ))}
-                    </div>
+            {/* Right Tabs */}
+            <div className="tab-column right">
+            {tabs.slice(6).map((tab) => (
+                <button
+                key={tab.name}
+                className={`tab-button right-tab ${activeTab === tab.name ? 'active' : ''}`}
+                onClick={() => setActiveTab(tab.name)}
+                >
+                <div className="tab-icon-wrapper">
+                    <img src={tab.blob} alt={`${tab.name} blob`} className="tab-blob" />
+                    <img src={tab.icon} alt={`${tab.name} icon`} className="tab-icon" />
+                </div>
+                <span className="tab-text">{tab.name}</span>
+                </button>
+            ))}
+            </div>
 
-                    {/* Right Tabs */}
-                    <div className="tab-column right">
-                        {tabs.slice(6).map((tab) => (
-                        <button
-                            key={tab}
-                            className={`tab-button right-tab ${activeTab === tab ? 'active' : ''}`}
-                            onClick={() => setActiveTab(tab)}
-                        >
-                            {tab}
-                        </button>
-                        ))}
-                    </div>
-                        <div className="card-grid-container">
-                            <div className="card-grid">
-                                {currentItems.map((p) => (
-                                <Card
-                                    key={p.id}
-                                    name={p.name}
-                                    role={p.role}
-                                    description={p.description}
-                                    profilepic={p.profilepic}
-                                />
-                                ))}
-                            </div>
-                        <div className="oral-histories-pagination">
-                            <Pagination pageCount={pageCount} handlePageClick={handlePageClick} />
-                        </div>
-                        </div>
-                    </div>
+            <div className="card-grid-container">
+                <div className="card-grid">
+                    {currentItems.map((p) => (
+                    <Card
+                        key={p.id}
+                        name={p.name}
+                        role={p.role}
+                        description={p.description}
+                        profilepic={p.profilepic}
+                    />
+                    ))}
+                </div>
+            <div className="oral-histories-pagination">
+                <Pagination pageCount={pageCount} handlePageClick={handlePageClick} />
+            </div>
+            </div>
+            </div>
 
-                          <div className="AdvancedSearch_border">  </div>
+            <div className="AdvancedSearch_border">  </div>
            { /*
            EXPLORE ORAL HISTORIES 
            <Stack direction='column' gap={4} margin={'5% 5% 5% 5%'} className="OralHistories_NWC_container">
